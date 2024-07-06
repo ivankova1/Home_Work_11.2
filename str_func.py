2ea701ff11749c972859f6461a02bb7898e62889
ivan kova1 <ivan.2002aus@gmail.com>
2024-07-06 17:16:36 +0300

    new function

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def convert_to_uppercase(string):
+    """ функция приводит все буквы строки к верхнему регистру"""
     return string.upper()
+
+def capitalize_string(string):
+    """ функция Приводиt первую букву строки к верхнему регистру"""
+    return string.capitalize()
